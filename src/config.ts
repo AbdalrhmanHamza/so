688a2b04913ef39c9f82a206350d540f9957c9f4
AbdalrhmanHamza <abdarlhmanabohamza151@gmail.com>
2022-12-07 22:00:12 +0200

    four

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Solutions Blog'
-export const SITE_DESCRIPTION = 'A blog for many topics'
+export const SITE_DESCRIPTION = 'A blog for many topic'
 export const HOMEPAGE_URL = 'https://www.solutions-blog.tech/'
