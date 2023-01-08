c39e0105869a27bcf096bc1ac52c28a3c1ae5538
AbdalrhmanHamza <abdarlhmanabohamza151@gmail.com>
2023-01-08 09:30:51 +0200

    dsajlfhel

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 
 export const SITE_TITLE = 'Solutions Blog'
 export const SITE_DESCRIPTION =
-  'Solutions Blog is a blog for many significant topics like cars, tutorials, and programming.'
+  'Solutions Blog is a blog for many significant topics like cars, tutorials, programming, Frontend Development, awesome. '
 export const HOMEPAGE_URL = 'https://www.solutions-blog.tech/'
 export const SITE_KEYWORDS =
   'Solutions Blog, Solutions, Blog, Programming, cars, Python, Javascript, HTML, CSS, React JS, tutorials'
