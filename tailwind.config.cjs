290fd28e977aed92448412d2290fafaabc7f4a55
AbdalrhmanHamza <abdarlhmanabohamza151@gmail.com>
2023-01-22 08:33:52 +0200

    saldfjasldj

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,15 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        gray: {
+          900: '#18181B',
+          800: '#000000'
+        },
+        warm: {
+          800: '#D9D9D9'
+        }
+      },
       fontFamily: {
         mplus: ["'M PLUS Rounded 1c'", 'Verdana', 'sans-serif']
       }
