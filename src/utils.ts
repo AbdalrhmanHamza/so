b3ccfcaf25340f3b03d034cbff15068f8d7b7ae2
AbdalrhmanHamza <abdarlhmanabohamza151@gmail.com>
2023-02-03 16:19:53 +0200

    djslafjsdalj

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 const formatDate = (createdAt: string): string => {
   return new Date(createdAt).toLocaleDateString('en-uk', {
+    weekday: 'long',
     year: 'numeric',
     month: 'long',
     day: 'numeric'
