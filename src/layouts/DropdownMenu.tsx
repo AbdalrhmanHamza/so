8e7b17b0233de9c7297c05ecc0feb24703429827
AbdalrhmanHamza <abdarlhmanabohamza151@gmail.com>
2023-03-11 07:30:48 +0200

    fdjsalfjsaldjf

--- src/layouts/DropdownMenu.tsx
+++ src/layouts/DropdownMenu.tsx
@@ -13,7 +13,7 @@ export default function Tags({ tags }: Props) {
             <a
               href={`/categories/${tag.toLowerCase()}`}
               target="_blank"
-              className="bg-blue-100 hover:bg-blue-200 text-blue-800 text-base font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-neutral-800 dark:hover:bg-neutral-600 dark:text-blue-100 border border-blue-400"
+              className="bg-blue-100 hover:bg-blue-200 text-blue-800 text-base font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-neutral-800 dark:hover:bg-neutral-600 dark:text-blue-400 border border-blue-400"
               key={tag}
             >
               {tag}
