1125c5bf75f2f2c48d825978bc039f8fdf216da7
AbdalrhmanHamza <abdarlhmanabohamza151@gmail.com>
2023-04-04 13:15:13 +0200

    jsdalfjwevlsirhlirhf

--- src/layouts/DropdownMenu.tsx
+++ src/layouts/DropdownMenu.tsx
@@ -15,6 +15,7 @@ export default function Tags({ tags }: Props) {
               target="_blank"
               className="bg-blue-100 hover:bg-blue-200 text-blue-800 text-base font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-neutral-800 dark:hover:bg-neutral-600 dark:text-blue-400 border border-blue-400"
               key={tag}
+              title={`Category: ${tag}`}
             >
               <p className="inline-block">{tag}</p>
             </a>
