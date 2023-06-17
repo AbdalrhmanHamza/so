93190019a3ed457d50288ba5136724e1b5edd65d
AbdalrhmanHamza <abdarlhmanabohamza151@gmail.com>
2023-06-17 11:29:27 +0300

    jsdalfjlsei

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = 'Frontend Solutions'
 export const SITE_DESCRIPTION =
-  'Frontend Solutions is a blog for HTML & CSS & JavaScript and React JS code examples and frontend Development tutorials'
+  'Frontend Solutions is a blog for (HTML, CSS, JavaScript, React JS, Vue JS and Jquery) templates, code examples and frontend Development tutorials'
 export const HOMEPAGE_URL = 'https://www.solutions-blog.tech'
 export const SITE_KEYWORDS =
   'Frontend Solutions, Frontend Development, code examples, frontend code examples, Javascript, HTML, CSS, React JS, tutorials'
